0447ad8db3ab405b3272a7623bcd3352ca2dc16e
ericzhang02 <eric.raymong.zhang@gmail.com>
2023-09-26 19:04:46 -0500

    enhance

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-// JavaScript to toggle the visibility of the example experiences
+// JavaScript for dynamic content and smooth scrolling
 
-const toggleButton = document.getElementById('toggle-example');
-const exampleList = document.getElementById('example-list');
+document.addEventListener("DOMContentLoaded", function () {
+    // Example experiences data
+    const exampleExperiences = [
+        "Example Experience 1",
+        "Example Experience 2",
+        "Example Experience 3",
+    ];
 
-toggleButton.addEventListener('click', () => {
-    if (exampleList.style.display === 'none' || exampleList.style.display === '') {
-        exampleList.style.display = 'block';
-    } else {
-        exampleList.style.display = 'none';
+    const exampleList = document.getElementById("example-list");
+
+    // Function to load example experiences
+    function loadExampleExperiences() {
+        exampleExperiences.forEach((experience) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = experience;
+            exampleList.appendChild(listItem);
+        });
     }
+
+    // Toggle button functionality
+    const toggleButton = document.getElementById("toggle-example");
+    toggleButton.addEventListener("click", () => {
+        if (exampleList.style.display === "none" || exampleList.style.display === "") {
+            exampleList.style.display = "block";
+            loadExampleExperiences(); // Load the experiences when shown for the first time
+        } else {
+            exampleList.style.display = "none";
+        }
+    });
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: "smooth",
+                });
+            }
+        });
+    });
 });
